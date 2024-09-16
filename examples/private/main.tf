f16d2464d34b6851775509fa7ee0ce25e651baec
Alex Reiff <alex.reiff@ibm.com>
2024-09-16 13:33:30 -0400

    fix: update other test

--- examples/private/main.tf
+++ examples/private/main.tf
@@ -22,7 +22,7 @@ module "private_secret_engine" {
   secrets_manager_guid      = module.secrets_manager.secrets_manager_guid
   region                    = var.region
   root_ca_name              = var.root_ca_name
-  root_ca_common_name       = "cloud.ibm.com"
+  root_ca_common_name       = "*.cloud.ibm.com"
   root_ca_max_ttl           = "8760h"
   intermediate_ca_name      = var.intermediate_ca_name
   certificate_template_name = var.certificate_template_name
