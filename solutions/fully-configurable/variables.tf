ec8415055decbd43fab4bb431aace8d034b02385
Aayush-Abhyarthi <Aayush.Abhyarthi@ibm.com>
2025-05-14 18:56:05 +0530

    update example in description

--- solutions/fully-configurable/variables.tf
+++ solutions/fully-configurable/variables.tf
@@ -16,7 +16,7 @@ variable "existing_secrets_manager_crn" {
 
 variable "prefix" {
   type        = string
-  description = "The prefix to be added to all resources created by this solution. To skip using a prefix, set this value to null or an empty string. The prefix must begin with a lowercase letter and may contain only lowercase letters, digits, and hyphens '-'. It should not exceed 16 characters, must not end with a hyphen('-'), and can not contain consecutive hyphens ('--'). Example: prod-0205-cos."
+  description = "The prefix to be added to all resources created by this solution. To skip using a prefix, set this value to null or an empty string. The prefix must begin with a lowercase letter and may contain only lowercase letters, digits, and hyphens '-'. It should not exceed 16 characters, must not end with a hyphen('-'), and can not contain consecutive hyphens ('--'). Example: prod-us-south."
 
   validation {
     condition = (var.prefix == null ? true :
