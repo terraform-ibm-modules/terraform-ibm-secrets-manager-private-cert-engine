37b2b9439d87bd6c95ce1f5db19fbdf278beda02
Terraform IBM Modules Operations <Terraform.IBM.Modules.Operations@ibm.com>
2025-07-13 00:01:42 +0000

    chore(deps): update terraform terraform-ibm-modules/secrets-manager/ibm to v2.6.9

--- examples/private/main.tf
+++ examples/private/main.tf
@@ -8,7 +8,7 @@ module "resource_group" {
 
 module "secrets_manager" {
   source               = "terraform-ibm-modules/secrets-manager/ibm"
-  version              = "2.6.8"
+  version              = "2.6.9"
   resource_group_id    = module.resource_group.resource_group_id
   region               = var.region
   secrets_manager_name = "${var.prefix}-secrets-manager"
