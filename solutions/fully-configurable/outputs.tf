e60de6d571b0daa16b8498c47765a7e09dbd0b38
Harika-Ponna <Harika.Ponna@ibm.com>
2025-11-05 11:33:12 +0530

    feat: Added next step urls for DA

--- solutions/fully-configurable/outputs.tf
+++ solutions/fully-configurable/outputs.tf
@@ -36,3 +36,18 @@ output "secrets_manager_crn" {
   description = "The CRN of the Secrets Manager instance"
   value       = var.existing_secrets_manager_crn
 }
+
+output "next_steps_text" {
+  value       = "Your Private Certificate Engine is ready."
+  description = "Next steps text"
+}
+
+output "next_step_primary_label" {
+  value       = "Go to Secrets Manager Private Certificates"
+  description = "Primary label"
+}
+
+output "next_step_primary_url" {
+  value       = "https://cloud.ibm.com/services/secrets-manager/${var.existing_secrets_manager_crn}?paneId=privateCertificates#/privateCertificates"
+  description = "Primary URL"
+}
