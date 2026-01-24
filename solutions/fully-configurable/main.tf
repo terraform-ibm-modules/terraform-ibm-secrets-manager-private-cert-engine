86b34f1109a7dd5eb3df37048fd31f0160a91e0e
Terraform IBM Modules Operations <106112202+terraform-ibm-modules-ops@users.noreply.github.com>
2026-01-24 04:16:18 +0000

    fix(deps): update dependencies (#421)

--- solutions/fully-configurable/main.tf
+++ solutions/fully-configurable/main.tf
@@ -8,7 +8,7 @@ locals {
 
 module "crn_parser" {
   source  = "terraform-ibm-modules/common-utilities/ibm//modules/crn-parser"
-  version = "1.3.7"
+  version = "1.4.1"
   crn     = var.existing_secrets_manager_crn
 }
 
